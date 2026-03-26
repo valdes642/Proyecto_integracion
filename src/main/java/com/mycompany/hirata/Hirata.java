cadba806bd64ae00de6ae5ee6341b0763d63d76c
valdes642 <vhvergarav@gmail.com>
2026-03-26 11:46:11 -0300

    Add files via upload

---
+++ src/main/java/com/mycompany/hirata/Hirata.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hirata;
+
+/**
+ *
+ * @author Victor Vergara
+ */
+public class Hirata {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
