5ea081dde129e7d1fe9c821ed4649d5afe98356b
RixhDev <103218414+RixhDev@users.noreply.github.com>
2026-04-16 11:06:50 -0400

    Update JU_Camion.java

--- src/test/java/Modelo/JU_Camion.java
+++ src/test/java/Modelo/JU_Camion.java
@@ -4,12 +4,22 @@
  */
 package Modelo;
 
+/*OLD--
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;*/
+import Modelo.Camion;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  *
